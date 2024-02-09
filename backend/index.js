c735540e770b38bb0dc215d90f795674e4970f75
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2024-02-09 15:48:59 +0700

    init port

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log(request)
+    return response.status(200).send('HelloWorld')
+});
+
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
 });
