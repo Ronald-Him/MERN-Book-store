5e0c422faef79074ccb4678c4911bfb3b0d35bb4
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2024-02-14 15:55:43 +0700

    init change

--- backend/index.js
+++ backend/index.js
@@ -18,9 +18,16 @@ app.post('/', async (request, response) => {
         ){
             return response.status(400).send({
                 message: "Send all required fields: title, author, publishYear",
-            })
+            });
         }
-        
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear,
+        }; 
+
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
     }
     catch(error){
         console.log(error.message);
