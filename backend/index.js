0eeb45481302325589c81a199c5758232f22e2a9
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2024-02-14 13:52:46 +0700

    init change

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,24 @@ app.get('/', (request, response) => {
     return response.status(200).send('HelloWorld')
 });
 
+app.post('/', async (request, response) => {
+    try{
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(400).send({
+                message: "Send all required fields: title, author, publishYear",
+            })
+        }
+        
+    }
+    catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+})
 
 
 mongoose.connect(mongoDBURL)
