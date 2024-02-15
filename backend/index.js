33cba5388d21b2c2ccfccd2de995adeda02cec1c
Ronald-Him <85689119+Ronald-Him@users.noreply.github.com>
2024-02-15 11:01:58 +0700

    init change get book by ID

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,34 @@ app.post('/books', async (request, response) => {
     }
 })
 
+//Route for Get all Books from database
+app.get('/books', async (request, response) => {
+    try{
+        const books = await Book.find({});
+
+        return response.status(200).json({
+            count: books.length,
+            data: books,
+        });
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+//Get book by ID in postman
+app.get('/books/:id', async (request, response) => {
+    try{
+
+        const { id } = request.params
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 mongoose.connect(mongoDBURL)
     .then(() => {
